13727e0b7579a42786913ee51c5e4fd6da990836
austindrew2021-code <Austin.drew2021@gmail.com>
2026-02-19 19:03:30 -0400

    Fix Stripe API version string

--- app/api/create-checkout-session.ts
+++ app/api/create-checkout-session.ts
@@ -9,7 +9,7 @@ if (!stripeSecretKey) {
 }
 
 const stripe = new Stripe(stripeSecretKey || '', {
-  apiVersion: '2024-06-20',
+  apiVersion: '2026-01-28.clover',
 });
 
 export default async function handler(req: any, res: any) {
