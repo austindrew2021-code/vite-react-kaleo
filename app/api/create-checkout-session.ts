b53dcd37ccb9375cb1df8347ddf596218db13667
austindrew2021-code <austin.drew2021@gmail.com>
2026-02-19 22:48:00 -0400

    Update create-checkout-session.ts

--- app/api/create-checkout-session.ts
+++ app/api/create-checkout-session.ts
@@ -9,7 +9,7 @@ if (!stripeSecretKey) {
 }
 
 const stripe = new Stripe(stripeSecretKey || '', {
-  apiVersion: '2026-01-28.clover',
+  apiVersion: '2024-12-18.acacia',
 });
 
 export default async function handler(req: any, res: any) {
